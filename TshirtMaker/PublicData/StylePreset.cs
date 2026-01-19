15d56c9512bb4d3f0ddd041abc366d2b48226572
Bhbored <bhbored2022@gmail.com>
2026-01-19 14:38:53 +0200

    adjusted some files

--- TshirtMaker/PublicData/StylePreset.cs
+++ TshirtMaker/PublicData/StylePreset.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using TshirtMaker.Models.Core;
 using TshirtMaker.Models.Enums;
 
-namespace TshirtMaker.Models.AI
+namespace TshirtMaker.PublicData
 {
     public class StylePreset : BaseEntity
     {
